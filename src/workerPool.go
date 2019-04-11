7bc52b90a5793e590289ae8c622364eae7948d91
jason.liu <jason.liu@petuum.com>
2019-04-11 14:49:32 -0700

    two version of implementation

--- src/workerPool.go
+++ src/workerPool.go
@@ -1,19 +1,95 @@
 package WorkerPool
 
-type WorkerPool struct {
+import time
+import context
+
+// define generic job type
+type JobType func(workload interface{}) (interface{})
+
+
+type interface WorkerPool {
   numWorkers int
-  jobs <-chan func(workload interface{}) (chan<- interface{})
+  jobs chan<- JobType
+  result <-chan interface{}
+  AddJob(interface{})
+  GetResult() interface{}
+}
+
+// ------- gorountine implementation ----------
+type WorkerPoolThread struct {
+  numWorkers int
+  jobs chan<- JobType
+  results <-chan interface{}
 }
 
 // new worker pool creates a new pool of workers where each worker will process using the provided func
-func NewWorkerPool(int num) &WorkerPool {
-  wp := &WorkerPool{num}
+func NewWorkerPoolThread(int num) &WorkerPoolThread {
+  wp := &WorkerPool{
+    num,
+    make(chan<- interface{}),
+    make(<-chan interface{})
+  }
   for w:=1; w <= num; w++ {
     go func(){
       for j := range wp.jobs{
-        j()
+        results<-j()
       }
     }
   }
   return wp
+}
+
+func (WorkerPoolThread* wp) AddJob(JobType job) {
+  wp.jobs <- job
+}
+
+func (WorkerPoolThread* wp) GetResult() interface{} {
+  return <-wp.results
+}
+
+
+
+
+// -------- semaphore implementation -----------
+type interface WorkerPoolSemaphore {
+  numberWorkers int
+  sem semaphore.NewWeighted
+  jobs chan<- JobType
+  results <-chan interface{}
+  ctx Context
+}
+
+func NewWorkerPoolSemaphore(int num) &NewWorkerPoolSemaphore {
+  return &WorkerPoolSemaphore{
+    num,
+    semaphore.NewWeighted(int64(num)),
+    make(chan<- JobType),
+    make(<-chan interface{}),
+    context.TODO()
+  }
+}
+
+// initialize the workpool
+func (WorkerPoolSemaphore* wp) Init() {
+  // keep executing jobs while there is job
+  for j := range wp.jobs {
+    // err when all resources are in use
+    if err := sem.Acquire(ctx, 1); err != nil {
+        log.Printf("Failed to acquire semaphore: %v", err)
+        break
+    }
+
+    go func() {
+      defer sem.Release(1)
+      wp.results <- j()
+    }
+  }
+}
+
+func (WorkerPoolSemaphore* wp) AddJob(JobType job) {
+  wp.jobs <- job
+}
+
+func (WorkerPoolSemaphore* wp) GetResult() interface{} {
+  return <- wp.results
 }
